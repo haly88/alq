4c5b56a8b05fee52fa33e7d496191e1ff7693be3
Haly88 <ramiromusso@gmail.com>
2015-12-04 20:15:58 -0300

    Nueva botonera

---
+++ app/helpers/botones_helper.rb
@@ -0,0 +1,27 @@
+module BotonesHelper
+
+	def boton_atras_root
+		link_to '<i class="fa fa-2x fa-angle-double-left"></i>'.html_safe, root_path, :title => "Atras"
+	end
+
+	def boton_atras_index
+		link_to '<i class="fa fa-2x fa-angle-double-left"></i>'.html_safe, root_path+controller_path, :title => "Atras"
+	end
+
+	def boton_nuevo
+		link_to '<i class="fa fa-2x fa-file-o"></i>'.html_safe, root_path+controller_path+'/new', :title => "Nuevo"
+	end
+
+	def boton_guardar
+		link_to '<i class="fa fa-2x fa-floppy-o"></i>'.html_safe, '#', title: "Guardar", id: "guardar"
+	end
+
+	def boton_borrar
+		link_to '<i class="fa fa-2x fa-trash-o"></i>'.html_safe, root_path+controller_path+'/'+params[:id], 
+		method: :delete, data: { confirm: 'Esta seguro?' }
+	end
+
+	def boton_cobrar
+		link_to '<i class="fa fa-2x fa-credit-card"></i>'.html_safe, new_liquidacion_path(contrato: @contrato), :title => "Cobrar"
+	end
+end
