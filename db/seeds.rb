63d249d79d5f158ed540ffd3e426b685b142d3c6
Haly88 <ramiromusso@gmail.com>
2015-12-29 16:32:30 -0300

    heroku 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+unless InmuebleTipo.any?
+	InmuebleTipo.create!(:id => 1, :codigo => 'DEP', :nombre => 'Departamento')
+	InmuebleTipo.create!(:id => 2, :codigo => 'CAS', :nombre => 'Casa')
+	InmuebleTipo.create!(:id => 3, :codigo => 'PH', :nombre => 'PH')
+	InmuebleTipo.create!(:id => 4, :codigo => 'CBC', :nombre => 'Countrie, Barrio Cerrado	')
+	InmuebleTipo.create!(:id => 5, :codigo => 'QUI', :nombre => 'Quinta')
+	InmuebleTipo.create!(:id => 6, :codigo => 'TLO', :nombre => 'Terreno, Lote')
+	InmuebleTipo.create!(:id => 7, :codigo => 'CAM', :nombre => 'Campo')
+	InmuebleTipo.create!(:id => 8, :codigo => 'GDE', :nombre => 'Galpon, Deposito')
+	InmuebleTipo.create!(:id => 9, :codigo => 'OFI', :nombre => 'Oficina')
+	InmuebleTipo.create!(:id => 10, :codigo => 'COC', :nombre => 'Cochera')
+end
+
+unless Empresa.any?
+	Empresa.create!(:id => 1)
+end
