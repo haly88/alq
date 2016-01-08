d0768c6f6e9d748e0b2e287512e189d67db55976
Haly88 <ramiromusso@gmail.com>
2016-01-07 23:17:07 -0300

    cuenta corriente 2

--- app/controllers/cuenta_corriente_controller.rb
+++ app/controllers/cuenta_corriente_controller.rb
@@ -1,1 +1,8 @@
 class CuentaCorrienteController < ApplicationController
+
+	def index
+		sql = "select nombre, codigo from personas"
+    @resultado = ActiveRecord::Base.connection.exec_query(sql)
+  end
+
+end
